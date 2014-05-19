51fa12f76dbe94e7bfac4f1de9a57b1eea7da2fc
Yuval <yvw.bor@gmail.com>
2014-05-19 18:48:23 +0300

    not doing ex2

---
+++ ex2.rb
@@ -0,0 +1,15 @@
+# Excersie 2: http://ruby.learncodethehardway.org/book/ex2.html
+
+# Since I'm not completely new to ruby or programming I won't do everything in every exercise
+# However I will probably still do the extra credit.
+
+#============================================================================================
+#= Extra Credit:
+#= 
+#= Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or pound character).
+#= Take your ex2.rb file and review each line going backwards. Start at the last line, and check each word in reverse against what you should have typed.
+#= Did you find more mistakes? Fix them.
+#= Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix them.
+#=============================================================================================
+
+puts "Sorry, I'm not doing this excercise :("
