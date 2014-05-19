2ca79d9f2376131c7df0455e5a5ce86e5f4b59c3
Yuval <yvw.bor@gmail.com>
2014-05-19 23:20:09 +0300

    ex23.. sort of done..?

---
+++ ex23.rb
@@ -0,0 +1,7 @@
+# Excersie 23: http://ruby.learncodethehardway.org/book/ex23.html
+
+# Freeform learning
+
+#============================================================================================
+
+# You know the drill...
