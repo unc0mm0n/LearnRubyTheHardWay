9efcd5c46b7f620fcf841354af58f88e795b9692
Yuval <yvw.bor@gmail.com>
2014-05-19 22:46:54 +0300

    ex22.. meh...

---
+++ ex22.rb
@@ -0,0 +1,8 @@
+# Excersie 22: http://ruby.learncodethehardway.org/book/ex22.html
+
+# Test time!
+
+#============================================================================================
+
+# Memorize memorize memorize!
+# Assume I did it :P
