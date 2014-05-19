0f5409d4c87c88b97e370b361b2279673511a7fc
Yuval <yvw.bor@gmail.com>
2014-05-19 20:05:53 +0300

    ex10 done

---
+++ ex10.rb
@@ -0,0 +1,20 @@
+# Excersie 10: http://ruby.learncodethehardway.org/book/ex10.html
+
+# I CAN'T TAKE THIS ANYMORE
+
+#============================================================================================
+#= Extra Credit:
+#= 
+#= None this time :)
+#=============================================================================================
+
+
+puts "too many \n\t god-damned \n\t\t prints!!"
+
+puts <<I_HATE_PRINTS
+This Section
+so\sgo\sto\shell\t!!!!
+\n\n\n
+I_HATE_PRINTS
+
+# Stress releaving
