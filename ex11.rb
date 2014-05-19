66ca6caa5ee690125f225e3670210a2d6a0b7e65
Yuval <yvw.bor@gmail.com>
2014-05-19 20:11:06 +0300

    ex11 done

---
+++ ex11.rb
@@ -0,0 +1,23 @@
+# Excersie 10: http://ruby.learncodethehardway.org/book/ex10.html
+
+# finally getting some input
+
+#============================================================================================
+#= Extra Credit:
+#= 
+#= None this time :)
+#=============================================================================================
+
+
+print "Who are you? "
+who = gets.chomp
+print "Who are you, really? "
+who2 = gets.chomp
+print "Stop lying, who are you? "
+who3 = gets.chomp
+print "seriously? "
+
+print "I don't think you are #{who3} or #{who2}, but there's no way in hell you are #{gets.chomp}!"
+
+# chomp removes the line_break character that is the enter key
+# gets.. well.. gets the input (Fun fact: gets is actually GET String)
