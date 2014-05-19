94e28cef47bbaa5aabb44768a4a4d368fcab41cb
Yuval <yvw.bor@gmail.com>
2014-05-19 23:35:11 +0300

    ex24 is not being done

---
+++ ex24.rb
@@ -0,0 +1,15 @@
+# Excersie 24: http://ruby.learncodethehardway.org/book/ex24.html
+
+# That's a stamina exercise. I am too lazy to work on stamina
+
+#============================================================================================
+#= Extra Credit:
+#= 
+=begin
+  
+nope, nothing here
+  
+=end
+#=============================================================================================
+
+puts "Sorry, I'm not doing this excercise :("
