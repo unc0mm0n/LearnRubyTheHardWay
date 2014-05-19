7ec90322a9e378ea39b7e4168f46a05705b65d7a
Yuval <yvw.bor@gmail.com>
2014-05-19 20:01:16 +0300

    ex9 done

---
+++ ex9.rb
@@ -0,0 +1,23 @@
+# Excersie 9: http://ruby.learncodethehardway.org/book/ex9.html
+
+# And even more printing...
+# note that \n is the sign of a new line. backslash \ have special use inside strings started with ""
+# and therefore should be used carefuly.
+# oh, and PARAGRAPHs
+
+#============================================================================================
+#= Extra Credit:
+#= 
+#= None this time :)
+#=============================================================================================
+
+days = "sunday\nmonday\ntruuuday\nfalseday\nrandomday\ncthulhuday\nsaturday\n"
+
+print days
+puts '--------'
+puts <<PARAGRAPH
+this is text
+and so is this
+PARAGRAPH
+
+# Ah, that's a nicer way of doing these exercises
